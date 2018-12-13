6c46787e572834a1881d83978a63dcfbf977728a
Shakleen Ishfar <shakleenishfar@gmail.com>
2018-12-13 20:43:56 +0600

    13 December, 2018 - Fixed scaling issue with new skill portion.

--- Logic.js
+++ Logic.js
@@ -1,44 +1,4 @@
-var Full = document.querySelector(".Full");                 // Skill Container is the entire block
-var Half = document.querySelectorAll(".Half");              // Skill Sub Container is half the block
 var Quarter = document.querySelectorAll(".Quarter");        // Skill Container is the entire block
-
-MakeAllQuartersInvisible = function() {                     // Makes all the quarters disappear
-    for (var i = 0; i < Quarter.length; ++i){
-        Quarter[i].classList.toggle('Invisible');
-    }
-}
-
-HalfClickEffect = function(index) {                         // Makes only the indexed half visible
-    for (var i = 0; i < Half.length; ++i){
-        Half[i].classList.toggle('Invisible');
-    }
-    Half[index].classList.remove('Invisible');
-    Half[index].classList.toggle('HalfClickEffect');
-}
-
-UpperHalf = function(){                                     // For the upper half quaters.
-    MakeAllQuartersInvisible();
-    this.classList.remove('Invisible');
-    this.classList.toggle('QuarterClickEffect');
-    HalfClickEffect(0);
-
-    var HiddenCells = this.querySelectorAll('.HiddenCell');
-    for (var i = 0; i < HiddenCells.length; ++i){
-        HiddenCells[i].classList.toggle('Invisible');
-    }
-}
-
-LowerHalf = function(){                                     // For the lower half quarters
-    MakeAllQuartersInvisible();
-    this.classList.remove('Invisible');
-    this.classList.toggle('QuarterClickEffect');
-    HalfClickEffect(1);
-
-    var HiddenCells = this.querySelectorAll('.HiddenCell');
-    for (var i = 0; i < HiddenCells.length; ++i){
-        HiddenCells[i].classList.toggle('Invisible');
-    }
-}
 
 HoverEffect = function (){
     for (var i = 0; i < Quarter.length; ++i){
@@ -48,6 +8,20 @@ HoverEffect = function (){
 }
 
 for (var i = 0; i < Quarter.length; ++i){
-    Quarter[i].addEventListener('click', (i < 2) ? UpperHalf : LowerHalf);
+    Quarter[i].addEventListener('click', function(){
+        for (var i = 0; i < Quarter.length; ++i){
+            Quarter[i].classList.toggle('Invisible');
+        }
+
+        this.classList.remove('Invisible');
+        this.classList.toggle('col-md-5');
+        this.classList.toggle('col-md-10');
+        this.classList.toggle('QuarterClickEffect');
+
+        var HiddenCells = this.querySelectorAll('.HiddenCell');
+        for (var i = 0; i < HiddenCells.length; ++i){
+            HiddenCells[i].classList.toggle('Invisible');
+        }
+    });
     Quarter[i].addEventListener('mouseover', HoverEffect);
 }
